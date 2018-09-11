068503c2929006d5d0b93581a1b340891f1de8dd
Tomohiro Ichimura <tichimura@pivotal.io>
2018-09-11 11:36:28 +0900

    f3 first commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 //import javax.servlet.ServletException;
 
 @SpringBootApplication
